16e4bda590e72ff74394455644d116e8cf760799
djihene-latoui <jiheen030@gmail.com>
2026-04-26 18:31:11 +0200

    Update next.config.ts

--- atlas-frontend/next.config.ts
+++ atlas-frontend/next.config.ts
@@ -12,7 +12,18 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${process.env.API_URL}/api/:path*`,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://atlas-front-virid.vercel.app" },
+        ],
       },
     ];
   },
